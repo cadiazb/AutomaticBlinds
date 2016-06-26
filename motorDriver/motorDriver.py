e12c66235953a6f257f9d3336d1228164b5a227c
Camilo <cadiazb@berkeley.edu>
2016-06-25 22:27:53 -0700

    Minor changes and lines to test blinds

--- motorDriver/motorDriver.py
+++ motorDriver/motorDriver.py
@@ -1,45 +1,71 @@
 #!/usr/bin/env python
 
 import subprocess
-#from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 
 class stepperMotor:
 	
 	def __init__(self,enable, direction, step):
-		#enablePin = LED(enable)
-		#directionPin = LED(direction)
-		#stepPin = LED(step)
-		stepLocation = 0
+		self.enablePin = LED(enable)
+		self.directionPin = LED(direction)
+		self.stepPin = LED(step)
+		self.stepLocation = 0
 
-		#enablePin.on() #Logic high disables motor
-		#directionPin.on() #Default direction is forward
-		#stepPin.off()
+		self.enablePin.on() #Logic high disables motor
+		self.directionPin.on() #Default direction is forward
+		self.stepPin.off()
 
-	def stepForward(self, steps, pause):
+	def rotate(self, steps, pause):
 		with open('motorStepLocation','r') as motorLocation:
 			for line in motorLocation:
-				stepLocation = int(line.split(None, 1)[0])
+				self.stepLocation = int(line.split(None, 1)[0])
 		motorLocation.close()
 
-		print stepLocation
-		#stepPin.off()
-		#enablePin.off() #Logic low enables motor
-		#directionPin.on() #Choose forward direction
+		print self.stepLocation
+		self.stepPin.off()
+		self.enablePin.off() #Logic low enables motor
+		if (steps > 0):
+			self.directionPin.on() #Choose forward direction
+			tmpForward = 1
+		else:
+			self.directionPin.off() #Choose reverse direction
+			tmpForward = -1
 
-		for iStep in range (0, steps):
-			#stepPin.on()
-			#sleep(pause)
-			#stepPin.off()
-			#sleep(pause)
-			stepLocation = stepLocation + 1
+		for iStep in range (0, abs(steps)):
+			self.stepPin.on()
+			sleep(pause)
+			self.stepPin.off()
+			sleep(pause)
+			self.stepLocation = self.stepLocation + tmpForward
 
-		#enablePin.on() #Logic high disables motor
+		self.enablePin.on() #Logic high disables motor
+		print self.stepLocation
 		subprocess.check_output("mv motorStepLocation motorStepLocation.old", shell = True)
-		subprocess.check_output("echo %i >> motorStepLocation" %stepLocation, shell = True)
+		subprocess.check_output("echo %i >> motorStepLocation" %self.stepLocation, shell = True)
+	
+	def gotToOrigin(self):
+		with open('motorStepLocation','r') as motorLocation:
+                        for line in motorLocation:
+                                self.stepLocation = int(line.split(None, 1)[0])
+                motorLocation.close()
 
+		self.rotate(-self.stepLocation, 0.01)
 
+	def getPosition(self):
+                with open('motorStepLocation','r') as motorLocation:
+                        for line in motorLocation:
+                                self.stepLocation = int(line.split(None, 1)[0])
+                motorLocation.close()
+
+                print self.stepLocation
+		return self.stepLocation
 
 if __name__ == "__main__":
-	myMotor = stepperMotor(1,2,3)
-	myMotor.stepForward(10,0.1)
+	myMotor = stepperMotor(25,23,24)
+	#myMotor.rotate(-500,0.025)
+	#print "Motor position is:"
+	sleep(1)
+	#myMotor.getPosition()
+	#myMotor.gotToOrigin()
+	myMotor.rotate(1000,0.04)
